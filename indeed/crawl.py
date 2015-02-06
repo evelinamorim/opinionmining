774ad71b7286fa7802382aafa95173be119fc1ed
Evelin Amorim <evelin.amorim@asteria.speed.dcc.ufmg.br>
2015-02-06 17:55:53 -0200

    iterar sobre xml

--- indeed/crawl.py
+++ indeed/crawl.py
@@ -1,17 +1,36 @@
 import urllib
 import urllib2
+from lxml import etree
 
-url = 'http://www.indeed.com.br/cmp'
+#url = 'http://www.indeed.com.br/cmp'
 
-empresa=MRS
-values = {'q':empresa}
-data = urllib.urlencode(values)
+def get_xml():
+    url = 'http://api.indeed.com/ads/apisearch'
 
-full_url = url + '?' + data
-#req = urllib2.Request(url)
-print full_url
-response = urllib2.urlopen(full_url)
+    #empresa='MRS'
+    cidade ='belo horizonte,mg'
+    pais = 'br'
+    values = {'l':cidade,'publisher':4636456854731054,'v':2,'co':pais}
+    data = urllib.urlencode(values)
 
-html = response.read()
+    full_url = url + '?' + data
+    #req = urllib2.Request(url)
+    #print full_url
+    response = urllib2.urlopen(full_url)
 
-print html
+    html = response.read()
+    
+    print html
+
+def process_xml(xml_file):
+    tree = etree.parse(xml_file)
+    q = [tree.getroot()]
+    while (q!=[]):
+        n = q.pop()
+        q  = q + n.getchildren()
+        if (n.tag == 'company'):
+          print ">>",n.text
+
+
+if __name__ == '__main__':
+    #process_xml('vagasbelohorizonte.xml')
